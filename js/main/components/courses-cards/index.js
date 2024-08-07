a118ab89ca77e9324c8aad6ba3a8620aa06f14cd
Khairul <Qai-rule@hotmail.com>
2024-08-07 18:04:18 +0800

    update: cleaned up file and fixed sizing issue

--- js/main/components/courses-cards/index.js
+++ js/main/components/courses-cards/index.js
@@ -1,17 +1,8 @@
-const isTablet = window.innerWidth < 992;
-const isMobile = window.innerWidth < 768;
-
-function getSlidesNumber() {
-    if (isMobile) return 1;
-    if (isTablet) return 2;
-    return 3;
-}
-
 function CoursesCards() {
     setTimeout(() => {
         var swiper = new Swiper(".course-swiper", {
             spaceBetween: 24,
-            slidesPerView: 3,
+            slidesPerView: 1,
             breakpoints: {
                 478: {
                     slidesPerView: 1,
@@ -36,14 +27,5 @@ function CoursesCards() {
         });
     }, 200);
 }
-console.log("CoursesCards");
-// window.addEventListener("resize", function () {
-//     var ww = $(window).width();
-//     console.log(ww);
-//     if (ww >= 992) Swiper.params.slidesPerView = 3;
-//     if (ww < 992) Swiper.params.slidesPerView = 2;
-//     if (ww < 768) Swiper.params.slidesPerView = 1;
-//     Swiper.reInit();
-// });
 
 document.addEventListener("DOMContentLoaded", CoursesCards);
